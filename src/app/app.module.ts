4cdea0d1c29e2dd182cb947b68a9573b43f70bc2
Carlos López <calopezs1110@gmail.com>
2024-01-29 09:21:12 -0500

    Docs creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
 import { CounterModule } from './counter/counter.module';
 import { HeroesModules } from './heroes/heroes.module';
 import { DbzModule } from './dbz/dbz.module';
@@ -12,7 +11,6 @@ import { DbzModule } from './dbz/dbz.module';
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
